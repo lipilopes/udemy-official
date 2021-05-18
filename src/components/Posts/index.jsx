2c5ae5c66e389d56a7b6a7fe1dc6552ce104cb32
lipilopes <felippe091234@gmail.com>
2021-05-18 18:32:52 -0300

    logic: add prop-types

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,15 +1,28 @@
+import P from 'prop-types';
+
+import React from 'react';
+
 import { PostCard } from '../PostCard';
 
 import './styles.css';
 
 export const Posts = ({ posts }) => (
-    <div className="posts">
-        {posts.map(post => (
-            <PostCard
-                title={post.title}
-                body={post.body}
-                cover={post.cover}
-                key={post.id} />
-        ))}
-    </div>
+  <div className="posts">
+    {posts.map((post) => (
+      <PostCard title={post.title} body={post.body} cover={post.cover} key={post.id} />
+    ))}
+  </div>
 );
+
+Posts.defaultProps = { posts: [] };
+
+Posts.propTypes = {
+  posts: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      cover: P.string.isRequired,
+      body: P.string.isRequired,
+      id: P.number.isRequired,
+    }),
+  ),
+};
--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,13 +1,20 @@
+import P from 'prop-types';
 import './styles.css';
-
+import React from 'react';
 
 export const TextInput = ({ searchValue, handleChange }) => {
-    return (
-        <input
-            className="text-input"
-            type="search"
-            onChange={handleChange}
-            placeholder="Escreve alguma coisa ai"
-        />
-    );
-}
+  return (
+    <input
+      className="text-input"
+      type="search"
+      value={searchValue}
+      onChange={handleChange}
+      placeholder="Escreve alguma coisa ai"
+    />
+  );
+};
+
+TextInput.propTypes = {
+  searchValue: P.string.isRequired,
+  handleChange: P.func.isRequired,
+};
